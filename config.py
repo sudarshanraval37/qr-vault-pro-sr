c05eedb927c3655182face629100e0c97802bf40
Fardeen The Developer <fardeenthedeveloper@gmail.com>
2026-02-14 10:19:54 +0530

    vercel tmp/ for write db

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     """Base configuration."""
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-12345')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///qr_vault.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/qr_vault.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
